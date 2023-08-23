2be3e957b0557bc97c4b4f3237f09b1fe2acead1
Moneshwar <76728384+Moneshwar@users.noreply.github.com>
2023-08-23 22:10:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function scrollToReviews()
+{
+    document.getElementById("reviews").scrollIntoView()
+}
+
+function scrollToCourses()
+{
+    document.getElementById("courses").scrollIntoView()
+}
+
+function buyNow(s)
+{
+    alert(s+" Course bought successfully")
+}
